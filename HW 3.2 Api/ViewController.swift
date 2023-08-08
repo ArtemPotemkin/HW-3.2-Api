0390ba3abd74b9230368027d415bdf376ac650a4
Artem Potemkin <art.potemkin@gmail.com>
2023-08-08 17:27:14 +0300

    Initial Commit

---
+++ HW 3.2 Api/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW 3.2 Api
+//
+//  Created by Артём Потёмкин on 08.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
